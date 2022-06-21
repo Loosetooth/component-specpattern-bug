566a616fc2d7d9699b5b75fa985509a7f1e2aaec
Loosetooth <loosetooth@protonmail.com>
2022-06-21 10:26:23 +0200

    add sample bug config

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   e2e: {
     setupNodeEvents(on, config) {},
+    specPattern: [
+      "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+      "pages/**/*.cy.{js,jsx,ts,tsx}",
+    ],
   },
-}
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "webpack",
+    },
+  },
+};
